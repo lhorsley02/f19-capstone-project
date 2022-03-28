b39a528aa1814f128f4f5c65d6caad263bf2721e
Laura Horsley <horsleyshorses02@gmail.com>
2022-03-28 15:46:12 -0700

    TEST

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const {plants} = require('./data')
+
 
 const app = express();
 app.use(cors());
@@ -9,24 +9,15 @@ app.use(express.json());
 
 
 
-// app.use(express.static(path.join(__dirname, "./indoor-plants")));
-// app.use(express.static(path.join(__dirname, "./succulents")));
-// app.use(express.static(path.join(__dirname, "./garden-plants")));
-// app.use(express.static(path.join(__dirname, "./all-plants")));
+app.use(express.static(path.join(__dirname, "./indoor-plants")));
+app.use(express.static(path.join(__dirname, "./succulents")));
+app.use(express.static(path.join(__dirname, "./garden-plants")));
+app.use(express.static(path.join(__dirname, "./all-plants")));
 app.use(express.static(path.join(__dirname, "./public")));
 
 // app.get('/', (req, res) => {
 //     res.sendFile(path.join(___dirname, '/index.html'))
 // })
-
-
-
-const {addPlant, deletePlant} = require('./controller')
-
-
-app.post('/api/my-plants', addPlant);
-app.delete('/api/my-plants/:id', deletePlant);
-
 
 
 
