29322dc3d3d74054e559e8aa1f5610bd7eb1bef4
Laura Horsley <horsleyshorses02@gmail.com>
2022-03-28 21:40:23 -0700

    Objects Created/Lists Edited

---
+++ data.js
@@ -0,0 +1,102 @@
+const plants = [
+    {
+        name: "Monstera Deliciosa",
+        sunlight: "Bright Indirect",
+        water: "Once Weekly",
+        category: "Indoor Plant"
+    
+    },
+    {
+        name: "Ficus Lyrata",
+        sunlight: "Bright Indirect",
+        water: "Every 7-10 Days",
+        category: "Indoor Plant"
+    },
+    {
+        name: "Pilea Peperomioides",
+        sunlight: "Bright Indirect",
+        water: "Once Weekly",
+        category: "Indoor Plant"
+    },
+    {
+        name: "Chlorophytum Comosum",
+        sunlight: "Low / Medium",
+        water: "Every 7-10 Days",
+        category: "Indoor Plant"
+    },
+    {
+        name: "Epipremnum Aureum",
+        sunlight: "Medium",
+        water: "Every 10 Days, When Dry",
+        category: "Indoor Plant"
+    },
+ 
+
+
+
+    {
+        name: "Sansevieria",
+        sunlight: "Anywhere from Low to Direct",
+        water: "Every 2-3 Weeks, When Dry",
+        category: "Succulent"
+    },
+    {
+        name: "Cactaceae",
+        sunlight: "Bright Indirect",
+        water: "10 - 14 Days, When Dry",
+        category: "Succulent"
+    },
+    {
+        name: "Crassula Ovata",
+        sunlight: "Bright Indirect",
+        water: "Every 2-3 Weeks, When Dry",
+        category: "Succulent"
+    },
+    {
+        name: "Zamioculcas",
+        sunlight: "Medium to Bright Indirect",
+        water: "Every 7-14 Days, When Dry",
+        category: "Succulent"
+    },
+    {
+        name: "Ceropegia",
+        sunlight: "Bright Indirect, Occasional Direct",
+        water: "Every 7-10 Days, When Dry",
+        category: "Succulent"
+    },
+    
+
+
+
+    {
+        name: "Lactuca Sativa",
+        sunlight: "Direct Sun",
+        water: "Daily",
+        category: "Garden Plant"
+    },
+    {
+        name: "Solanum Lycopersicum",
+        sunlight: "Direct",
+        water: "Daily",
+        category: "Garden Plant"
+    },
+    {
+        name: "Cucumis Sativus",
+        sunlight: "Direct",
+        water: "Daily",
+        category: "Garden Plant"
+    },
+    {
+        name: "Daucus Carota",
+        sunlight: "Direct",
+        water: "Daily",
+        category: "Garden Plant"
+    },
+    {
+        name: "Zea Mays",
+        sunlight: "Direct",
+        water: "Daily",
+        category: "Garden Plant"
+    },
+   
+]
