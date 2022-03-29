2459bd212ae31db6005e8241a5e53077325b920a
Laura Horsley <horsleyshorses02@gmail.com>
2022-03-29 10:32:39 -0700

    Started Axios Requests

--- public/index.js
+++ public/index.js
@@ -1,10 +1,50 @@
 
 
-function TestsFunction() {
-    let T = document.getElementById("monstera-box"),
-    displayValue = "";
-    if (T.style.display == "")
-    displayValue = "none";
+
+const monsteraBtn = document.querySelector('.monstera-view-btn')
+const fiddleBtn = document.querySelector('.fiddle-view-btn')
+const moneyPlantBtn = document.querySelector('.money-plant-view-btn')
+const pothosBtn = document.querySelector('.pothos-view-btn')
+const spiderBtn = document.querySelector('.spider-view-btn')
+
+
+const snakeBtn = document.querySelector('.snake-view-btn')
+const cactusBtn = document.querySelector('.cactus-view-btn')
+const jadeBtn = document.querySelector('.jade-view-btn')
+const zzBtn = document.querySelector('.zz-view-btn')
+const heartsBtn = document.querySelector('.hearts-view-btn')
+
+
+const lettuceBtn = document.querySelector('.lettuce-view-btn')
+const tomatoesBtn = document.querySelector('.tomatoes-view-btn')
+const cucumbersBtn = document.querySelector('.cucumbers-view-btn')
+const carrotsBtn = document.querySelector('.carrots-view-btn')
+const cornBtn = document.querySelector('.corn-view-btn')
+
+
+const BaseURL = `http://localhost:4005`
+
+
+function sayHello(event) {
     
-    T.style.display = displayValue;
+    axios.get(`${BaseURL}`)
+        .then((res) => {
+            alert("Working")
+        })
 }
+
+function getInfo(event) {
+    
+    axios.get(`${BaseURL}/plants/`)
+        .then(res => {
+            alert("working?")
+        })
+        .catch(err => {
+            console.log(err)
+        })
+}
+
+
+
+monsteraBtn.addEventListener('click', sayHello)
+fiddleBtn.addEventListener('click', getInfo)
--- public/server.js
+++ public/server.js
@@ -1,5 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+
+app.use(express.static(path.join(__dirname, "./public")));
+
+
+
 const plants = [
     {
+        id: 0,
         name: "Monstera Deliciosa",
         sunlight: "Bright Indirect",
         water: "Once Weekly",
@@ -7,24 +23,28 @@ const plants = [
     
     },
     {
+        id: 1,
         name: "Ficus Lyrata",
         sunlight: "Bright Indirect",
         water: "Every 7-10 Days",
         category: "Indoor Plant"
     },
     {
+        id: 2,
         name: "Pilea Peperomioides",
         sunlight: "Bright Indirect",
         water: "Once Weekly",
         category: "Indoor Plant"
     },
     {
+        id: 3,
         name: "Chlorophytum Comosum",
         sunlight: "Low / Medium",
         water: "Every 7-10 Days",
         category: "Indoor Plant"
     },
     {
+        id: 4,
         name: "Epipremnum Aureum",
         sunlight: "Medium",
         water: "Every 10 Days, When Dry",
@@ -35,30 +55,35 @@ const plants = [
 
 
     {
+        id: 5,
         name: "Sansevieria",
         sunlight: "Anywhere from Low to Direct",
         water: "Every 2-3 Weeks, When Dry",
         category: "Succulent"
     },
     {
+        id: 6,
         name: "Cactaceae",
         sunlight: "Bright Indirect",
         water: "10 - 14 Days, When Dry",
         category: "Succulent"
     },
     {
+        id: 7,
         name: "Crassula Ovata",
         sunlight: "Bright Indirect",
         water: "Every 2-3 Weeks, When Dry",
         category: "Succulent"
     },
     {
+        id: 8,
         name: "Zamioculcas",
         sunlight: "Medium to Bright Indirect",
         water: "Every 7-14 Days, When Dry",
         category: "Succulent"
     },
     {
+        id: 9,
         name: "Ceropegia",
         sunlight: "Bright Indirect, Occasional Direct",
         water: "Every 7-10 Days, When Dry",
@@ -69,30 +94,35 @@ const plants = [
 
 
     {
+        id: 10,
         name: "Lactuca Sativa",
         sunlight: "Direct Sun",
         water: "Daily",
         category: "Garden Plant"
     },
     {
+        id: 11,
         name: "Solanum Lycopersicum",
         sunlight: "Direct",
         water: "Daily",
         category: "Garden Plant"
     },
     {
+        id: 12,
         name: "Cucumis Sativus",
         sunlight: "Direct",
         water: "Daily",
         category: "Garden Plant"
     },
     {
+        id: 13,
         name: "Daucus Carota",
         sunlight: "Direct",
         water: "Daily",
         category: "Garden Plant"
     },
     {
+        id: 14,
         name: "Zea Mays",
         sunlight: "Direct",
         water: "Daily",
@@ -100,3 +130,22 @@ const plants = [
     },
    
 ]
+
+
+
+
+
+app.get('/plants', getInfo)
+app.post('/plants', displayInfo)
+
+
+
+
+const port = process.env.PORT || 4005;
+
+app.listen(port, () => {
+    console.log(`We vibin on port ${port}`)
+});    
+
+
+
